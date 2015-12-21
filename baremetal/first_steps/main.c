27bdea91ce433b45fac1e500386fd1a5dccf516a
Mikhail Tyryshkin <mikhail.tyryshkin@eltex.loc>
2015-12-22 02:15:55 +0600

    bare_metal: first steps completed

--- baremetal/first_steps/main.c
+++ baremetal/first_steps/main.c
@@ -1,7 +1,110 @@
-#include <stdio.h>
+
+#define GPIO_BASE 0x3F200000UL
+
+#define GPIO_GPFSEL0    0
+#define GPIO_GPFSEL1    1
+#define GPIO_GPFSEL2    2
+#define GPIO_GPFSEL3    3
+#define GPIO_GPFSEL4    4
+#define GPIO_GPFSEL5    5
+
+#define GPIO_GPSET0     7
+#define GPIO_GPSET1     8
+
+#define GPIO_GPCLR0     10
+#define GPIO_GPCLR1     11
+
+#define GPIO_GPLEV0     13
+#define GPIO_GPLEV1     14
+
+#define GPIO_GPEDS0     16
+#define GPIO_GPEDS1     17
+
+#define GPIO_GPREN0     19
+#define GPIO_GPREN1     20
+
+#define GPIO_GPFEN0     22
+#define GPIO_GPFEN1     23
+
+#define GPIO_GPHEN0     25
+#define GPIO_GPHEN1     26
+
+#define GPIO_GPLEN0     28
+#define GPIO_GPLEN1     29
+
+#define GPIO_GPAREN0    31
+#define GPIO_GPAREN1    32
+
+#define GPIO_GPAFEN0    34
+#define GPIO_GPAFEN1    35
+
+#define GPIO_GPPUD      37
+#define GPIO_GPPUDCLK0  38
+#define GPIO_GPPUDCLK1  39
+
+#define LED_GPFSEL      GPIO_GPFSEL4
+#define LED_GPFBIT      21
+#define LED_GPSET       GPIO_GPSET1
+#define LED_GPCLR       GPIO_GPCLR1
+#define LED_GPIO_BIT    15
 
 
+/* GPIO Register set */
+volatile unsigned int* gpio;
+
+/* Simple loop variable */
+volatile unsigned int tim;
+
+
+
+int main(void) __attribute__((naked));
 int main(void)
 {
+	/* Assign the address of the GPIO peripheral (Using ARM Physical Address) */
+	gpio = (unsigned int *)GPIO_BASE;
+
+    /* Write 1 to the GPIO47 init nibble in the Function Select 4 GPIO
+           peripheral register to enable GPIO47 as an output */
+    gpio[LED_GPFSEL] = (1 << LED_GPFBIT);
+
+	while(1)
+	{
+		for(tim = 0; tim < 500000; tim++);
+
+        /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on
+                   for plus models )*/
+        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+
+        for(tim = 0; tim < 500000; tim++);
+
+        /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off
+                   for plus models )*/
+        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+
+        for(tim = 0; tim < 100000; tim++);
+
+        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+
+        for(tim = 0; tim < 100000; tim++);
+
+        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+
+        for(tim = 0; tim < 100000; tim++);
+
+        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+
+        for(tim = 0; tim < 100000; tim++);
+
+        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+    }
+
 	return 0;
+}
+
+void exit(int code)
+{
+	while(1)
+	{
+
+	}
 }
