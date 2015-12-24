15b741367b223092dcf74251d3db8ade98c45785
Ardameon <helldiver@yandex.ru>
2015-12-24 23:16:48 +0600

    bare_metal: part2 extended

--- baremetal/build.sh
+++ baremetal/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+MAINDIR=`pwd`
 PART1=first_steps
+PART2=part2
 
 cd $PART1
 make
+cd $MAINDIR
+
+cd $PART2
+make
+cd $MAINDIR
--- baremetal/part2/part2.c
+++ baremetal/part2/part2.c
@@ -2,7 +2,7 @@
 
 
 /* GPIO Register set */
-volatile unsigned int* gpio;
+volatile unsigned int* gpio = (unsigned int *)GPIO_BASE;
 
 /* Simple loop variable */
 volatile unsigned int tim;
@@ -12,9 +12,6 @@ volatile unsigned int tim;
 int main(void) __attribute__((naked));
 int main(void)
 {
-	/* Assign the address of the GPIO peripheral (Using ARM Physical Address) */
-	gpio = (unsigned int *)GPIO_BASE;
-
     /* Write 1 to the GPIO47 init nibble in the Function Select 4 GPIO
            peripheral register to enable GPIO47 as an output */
     gpio[LED_GPFSEL] = (1 << LED_GPFBIT);
@@ -25,29 +22,13 @@ int main(void)
 
         /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on
                    for plus models )*/
-        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
 
         for(tim = 0; tim < 500000; tim++);
 
         /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off
                    for plus models )*/
-        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
-
-        for(tim = 0; tim < 100000; tim++);
-
         gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
-
-        for(tim = 0; tim < 100000; tim++);
-
-        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
-
-        for(tim = 0; tim < 100000; tim++);
-
-        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
-
-        for(tim = 0; tim < 100000; tim++);
-
-        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
     }
 
 	return 0;
